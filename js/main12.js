7db19e50f54903cd31e997f4fd69cfaaa6dd9c88
Kingslayer13 <3700511l>
2013-12-09 21:01:22 +0200

    require js

--- js/main12.js
+++ js/main12.js
@@ -1,3 +1,13 @@
 /**
  * Created by оо on 09.12.13.
  */
+require.config({
+    baseUrl: 'js/lib',
+    paths: {
+        jquery: "http://code.jquery.com/jquery-1.10.1.min"
+    }
+});
+
+require(['jquery'], function( jq ) {
+    console.log( jq ) // OK
+});
